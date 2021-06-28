e9465a8aab56ce0ee9f5192496b844ab9b96d77d
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2021-06-28 17:13:20 -0300

    Aprimora do exercicio 'rna-transcription'

--- exercicios/rna-transcription/lib/rna_transcription.ex
+++ exercicios/rna-transcription/lib/rna_transcription.ex
@@ -8,25 +8,14 @@ defmodule RnaTranscription do
   'UGAC'
   """
   @dna_to_rna %{
-    # 'G' -> 'C'
-    71 => 67,
-
-    # 'C' -> 'G'
-    67 => 71,
-
-    # 'T' -> 'A'
-    84 => 65,
-
-    # 'A' -> 'U'
-    65 => 85
+    ?G => ?C,
+    ?C => ?G,
+    ?T => ?A,
+    ?A => ?U
   }
 
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    Enum.map(dna,
-      fn char ->
-        @dna_to_rna[char]
-      end
-    )
+    Enum.map(dna, &(@dna_to_rna[&1]))
   end
 end
